61c4f5d10700a21eb4b8955ef543698a4978d354
Mkhululi <boymotsa@gmail.com>
2025-05-12 15:30:55 +0200

    an upload data amount has been added

--- app.js
+++ app.js
@@ -9,8 +9,8 @@ const app = express();
 //middleware
 app.use(cors());
 app.use(morgan("dev"));
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ limit: "20mb" }));
+app.use(express.urlencoded({ limit: "20mb", extended: true }));
 app.use("/api/images", express.static(path.join(__dirname, "public/images")));
 
 // routes block
@@ -18,13 +18,13 @@ const users = require("./routes/usersRoute");
 const auth = require("./routes/authRoute");
 const orders = require("./routes/orderRoute");
 const products = require("./routes/productRoute");
-const contact = require("./routes/contactRouter") 
+const contact = require("./routes/contactRouter");
 
 app.use("/api/users", users);
 app.use("/api/auth", auth);
 app.use("/api/orders", verifyToken.verifyToken, orders);
 app.use("/api/products", products);
-app.use("/api/contact" , contact)
+app.use("/api/contact", contact);
 
 //404 Error hangling
 app.use((req, res, next) => {
